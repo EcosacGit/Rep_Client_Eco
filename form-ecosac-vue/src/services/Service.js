ae34a48b0512cc78af98134124b724bb4d0026ab
jsoto <ecosacgitlab@gmail.com>
2024-05-24 17:04:07 -0500

    arreglos para conexion

--- form-ecosac-vue/src/services/Service.js
+++ form-ecosac-vue/src/services/Service.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 // -------------------- PRUEBAS---------------------//
-const baseURL = "http://127.0.0.1:8000/api/"; //API DE LARAVEL DE
+//const baseURL = "http://127.0.0.1:8000/api/"; //API DE LARAVEL DE
+//const baseURL = "http://10.10.100.155:8087/api_form_client/public/api/";
+//const baseURL = "http://api.ecosac.com.pe:58000/api_form_client/public/api/";
+const baseURL = "http://190.119.54.14:58000/api_form_client/public/api/";
 
 export const api = axios.create({
   baseURL,
